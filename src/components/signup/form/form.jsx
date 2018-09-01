109230f0dd349fdc5d9a2eff949ac474f51e0ddf
AndersonFeernandes <andersonlcfernandes@gmail.com>
2018-09-01 16:37:15 -0300

    Class to google auto-complete

--- src/components/signup/form/form.jsx
+++ src/components/signup/form/form.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { challanges } from "./mock";
 import styles from "./form.module.css";
+import Geosuggest from 'react-geosuggest';
+import { GoogleSearch } from "../../../shared/google/autoComplete";
 
 export default class SignupForm extends React.Component {
 
@@ -14,23 +16,30 @@ export default class SignupForm extends React.Component {
 
     componentWillMount() {
         challanges.then((response) => {
-            console.log(response);
             this.setState({ challanges: response })
 
         }).catch((err) => {
-            console.log(err);
+            console.error(err);
         })
+
+
+
     }
+
+    handleSearch(e) {
+
+    }
+
 
     render() {
         return (
             <form className="col-2 mx-auto">
                 <div className="form-group">
-                    <label>Phone number (incl. country code) of contact person</label>
+                    <label>Select a Challange</label>
                     <select className="input-block">
-                        <option value="" disabled selected hidden>Select a Challange</option>
+                        <option defaultValue="" disabled hidden>Select a Challange</option>
                         {this.state.challanges.map(challange =>
-                            <option value={challange.id}>{challange.title}</option>
+                            <option key={challange.id} value={challange.id}>{challange.title}</option>
                         )}
                     </select>
                 </div>
@@ -46,42 +55,46 @@ export default class SignupForm extends React.Component {
                 </div>
 
                 <div className="form-group">
-                    <label>Phone number (incl. country code) of contact person</label>
+                    <label>Email of contact person</label>
                     <input className="input-block" placeholder="Email of contact person" />
                 </div>
 
                 <div className="form-group">
-                    <label>Phone number (incl. country code) of contact person</label>
+                    <label>Company registration name</label>
                     <input className="input-block" placeholder="Company registration name" />
                 </div>
 
                 <div className="form-group">
-                    <label>Phone number (incl. country code) of contact person</label>
+                    <label>Date of Incorporation</label>
                     <input className="input-block" placeholder="Date of Incorporation" />
                 </div>
 
                 <div className="form-group">
-                    <label>Phone number (incl. country code) of contact person</label>
-                    <input className="input-block" placeholder="Company address" />
+                    <label>Company address</label>
+                    <GoogleSearch input={<input ref={(input) => this.search = input}></input>} onOpen={this.handleSearch} onClose={this.handleSearch} className="input-block" placeholder="Company address" >
+
+                    </GoogleSearch>
+
+
                 </div>
 
                 <div className="form-group">
-                    <label>Phone number (incl. country code) of contact person</label>
+                    <label>VAT number (Company registration number)</label>
                     <input className="input-block" placeholder="VAT number (Company registration number)" />
                 </div>
 
                 <div className="form-group">
-                    <label>Phone number (incl. country code) of contact person</label>
+                    <label>Short description of the company</label>
                     <textarea className={"input-block " + styles.textarea} placeholder="Short description of the company" />
                 </div>
 
                 <div className="form-group">
-                    <label>Phone number (incl. country code) of contact person</label>
+                    <label>Link to company website</label>
                     <input className="input-block" placeholder="Link to company website" />
                 </div>
 
                 <div className="form-group">
-                    <label>Phone number (incl. country code) of contact person</label>
+                    <label>Link to product/service video (Youtube, Vimeo, etc.)</label>
                     <input className="input-block" placeholder="Link to product/service video (Youtube, Vimeo, etc.)" />
                 </div>
 
